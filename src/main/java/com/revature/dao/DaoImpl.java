0dc72a65f79c8c2db4b3f11e811ef80204b71526
Clement Lin <x4383lin@gmail.com>
2017-10-01 02:19:33 -0400

    DaoImpl createUser()

--- src/main/java/com/revature/dao/DaoImpl.java
+++ src/main/java/com/revature/dao/DaoImpl.java
@@ -1,5 +1,46 @@
 package com.revature.dao;
 
-public class DaoImpl {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import com.revature.domain.Account;
+import com.revature.domain.User;
+
+public class DaoImpl implements Dao {
+	
+	private String url = "jdbc:oracle:thin:@localhost:1521:xe";
+	private String username = "banking_db";
+	private String password = "p4ssw0rd";
+	
+	// U_FN, U_LN, U_USERNAME, U_PASSWORD
+	public int createUser(User user) {
+		int status = 0;
+
+		try(Connection conn = DriverManager.getConnection(url, username, password);) {
+			String sql = "INSERT INTO bank_user (U_FN, U_LN, U_USERNAME, U_PASSWORD) values(?,?,?,?)";
+			
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1, user.getFirstName());
+			ps.setString(2, user.getLastName());
+			ps.setString(3, user.getUserName());
+			ps.setString(4, user.getPassword());
+			
+			
+			status = ps.executeUpdate(); // automatically commit;
+			System.out.println("Status: " + status);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return status;
+
+
+		
+	}
+
+	public int createAccount(Account acc) {
+		return 0;
+	}
 
 }
