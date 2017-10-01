66bf77240b50e90a978907cdf7857c423e6793a5
Clement Lin <x4383lin@gmail.com>
2017-10-01 02:03:29 -0400

    Dao 'create' methods

--- src/main/java/com/revature/dao/Dao.java
+++ src/main/java/com/revature/dao/Dao.java
@@ -1,5 +1,27 @@
 package com.revature.dao;
 
-public interface Dao {
+import com.revature.domain.Account;
+import com.revature.domain.User;
 
+public interface Dao {
+	/*
+	 * 	 CRUD methods only!!
+	 */
+		
+		//Create
+		public int createUser(User user);
+		public int createAccount(Account acc);
+		
+		
+		//READ
+		//public List<FlashCard> getAllFlashCard();
+		//public FlashCard getFlashCardByid(int id);
+		//public FlashCard getFlashCardByQuestion(String question);
+		
+		
+		//UPDATE
+		//public int updateFlashCard(FlashCard fc);
+		
+		//DELETE
+		//public int deleteFlashCard(FlashCard fc);
 }
