3a7170c2e044215b62cb3998d7e9cc3c8854fe60
Clement Lin <x4383lin@gmail.com>
2017-10-01 02:35:22 -0400

    main class test case

--- src/main/java/com/revature/main/Main.java
+++ src/main/java/com/revature/main/Main.java
@@ -1,10 +1,22 @@
 package com.revature.main;
 
+import com.revature.dao.Dao;
+import com.revature.dao.DaoImpl;
+import com.revature.domain.User;
+import com.revature.service.Service;
+
 public class Main {
 	
 	
 	
 	public static void main(String[] args) {
+		//User user = new User();
+		Service service = new Service();
+
+		//test
+		User user = new User("Clement","Lin","Clement1229","p4ssw0rd");
+		Dao dao = new DaoImpl();
+		System.out.println("Saving the new user:" + dao.createUser(user));
 		
 	}
 }
